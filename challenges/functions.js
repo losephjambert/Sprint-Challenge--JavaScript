ea041bb3ad0beb5370e764f7519c06380262d4d6
Joe <josephc.lambert@gmail.com>
2019-08-23 08:58:34 -0700

    adds closure answer

--- challenges/functions.js
+++ challenges/functions.js
@@ -16,19 +16,30 @@ const consume = (a, b, cb) => {
  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
  */
 const add = (num1, num2) => num1 + num2;
-const mulitply = (num1, num2) => num1 * num2;
+const multiply = (num1, num2) => num1 * num2;
 const greeting = (firstName, lastName) => `Hello ${firstName} ${lastName}, nice to meet you!`;
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume('Mary', 'Poppins', greeting)); // Hello Mary Poppins, nice to meet you!
 
 // ==== Closures ====
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
 // Explanation:
+
+/**
+ * nestedFunction() can access the variable `internal` because nestedFunction's scope
+ * includes myFunction's scope. nestedFunction has access to everything defined and/or
+ * assigned inside myFunction, however myFunction does not have access to what is
+ * defined and/or assigned inside nestedFunction.
+ *
+ * Of course, it also depends on where our code invokes nestedFunction.
+ *
+ * I've added a couple more lines of code below that illustrate how closures work.
+ */
 
 const external = "I'm outside the function";
 
@@ -39,6 +50,7 @@ function myFunction() {
   function nestedFunction() {
     console.log(internal);
   }
-  nestedFunction();
+  nestedFunction(); // we can invoke nestedFunction here becuase we're within the scope in which we declared nestedFunction()
 }
-myFunction();
+myFunction(); // invoking myFunction in turn invokes nestedFunction on line 53. If we comment this line out, nestedFunction is no longer invoked
+nestedFunction(); // we can't invoke nestedFunction() here because we don't have access to it in the global scope

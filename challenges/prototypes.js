d27b9e84b5275c2dad38f2dd013babc2fcbe6204
Joe <josephc.lambert@gmail.com>
2019-08-23 09:49:14 -0700

    adds surfaceArea method and tests pass

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -28,6 +28,11 @@ console.log(test_CuboidMaker.volume());
 
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
+CuboidMaker.prototype.surfaceArea = function() {
+  const { cLength, cWidth, cHeight } = this;
+  return 2 * (cLength * cWidth + cLength * cHeight + cWidth * cHeight);
+};
+console.log(test_CuboidMaker.surfaceArea());
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
